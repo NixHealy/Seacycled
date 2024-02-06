3347ff736f651a0351b1d98520bb1816b4f8fffd
Alex <2103334@uad.ac.uk>
2024-02-06 11:31:21 +0000

    first commit with test comment

---
+++ coral.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+var health = 100
+
+signal died
+
+func _process(delta):
+	%Sprite2D.material.set_shader_parameter("desaturate_strength", health / 100.0) #makes it less saturated
+														#probably change to different texture instead when we have the art
+	
+	var touching = %Area2D.get_overlapping_bodies().size() #how many enemies are touching it
+	health -= touching * delta * 10 #more enemies means loses health faster
+	
+	%Label.set_text("Health: " + str(round(health)))
+
+	if health <= 0: #oh no its dead
+		queue_free()
+		died.emit() #let other things know its dead
---
+++ player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+var norm_tex = load("res://img/fish.png")
+var att_tex = load("res://img/attack.png")
+
+var attacking = false
+
+func _physics_process(delta):
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT): #might wanna change this so its a quick attack
+		$Fish.set_texture(att_tex)						#instead of lasting as long as you hold it
+		attacking = true
+	else:
+		$Fish.set_texture(norm_tex)
+		attacking = false
+	
+	var direction
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT): 
+		direction = get_viewport().get_mouse_position() - get_global_transform_with_canvas().get_origin()
+		velocity = direction * (log(direction.length()) / log(10)) #faster when mouse is futher away
+		move_and_slide()
+		
+		#for above, consider changing so its more of a click and go here
+		#instead of a hold and continuously move towards it
+		#since constantly holding down hurts fingers
+		
+		#rotation = atan(velocity.y / velocity.x) #removed because it wasnt playing nice with collision
+		#rotation_degrees = snapped(rotation_degrees, 45)
+		if velocity.x > 0:
+			$Fish.flip_h = true
+		else:
+			$Fish.flip_h = false
+			
+		for body in %Area2D.get_overlapping_bodies(): #for everything nearby
+			if attacking == true:
+				if body.has_method("die"): #just to check
+					body.die() #MURDER
