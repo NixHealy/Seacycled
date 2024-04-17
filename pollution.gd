c44f6ab991033d10561e7d25208a185da7c4a341
alexjaycochrane <98971626+alexjaycochrane@users.noreply.github.com>
2024-04-17 14:43:33 +0100

    added images for trash

--- pollution.gd
+++ pollution.gd
@@ -4,12 +4,13 @@ var moving = true
 
 func _ready():
 	health = 1
-
+	
 	if FileAccess.file_exists("user://options.ini"):
 		config.load("user://options.ini")
 		var contrast = false
 		contrast = config.get_value("Options", "contrast")
 		%Sprite2D.material.set_shader_parameter("active", contrast)
+		%Sprite2D.set_texture("res://img/trash_bottle.PNG")
 
 func _physics_process(delta):
 		
@@ -22,6 +23,7 @@ func _physics_process(delta):
 	var targetPosition = global_position.direction_to(coral.global_position)
 	var direction = targetPosition #goes to the coral
 	
+	# Some extra math to add variety to the pollution spawn and direction
 	direction.x -= direction.x / 2
 	velocity = direction * 200.0
 	
